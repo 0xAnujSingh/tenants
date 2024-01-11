681a1d50059a4d217a0eec413359c5f26e923cf9
Anuj Singh Solanki <anujsinghsolanki1999@gmail.com>
2024-01-11 11:52:08 +0530

    In RoomService.js, manipulate documents of Rooms in Firestore

---
+++ src/components/RoomService.js
@@ -0,0 +1,45 @@
+import { db } from "../firebase";
+import {
+  collection,
+  getDoc,
+  getDocs,
+  doc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const collectionName = "Rooms";
+const roomCollectionRef = collection(db, collectionName);
+
+class RoomService {
+  ref = () => roomCollectionRef;
+
+  addRoom = (colRef, newRoom) => {
+    return addDoc(
+      colRef,
+      Object.assign({ CreatedAtDate: new Date() }, newRoom)
+    );
+  };
+
+  updateRoom = (id, updateRoom) => {
+    const roomDoc = doc(db, "Rooms", id);
+    return updateDoc(roomDoc, updateRoom);
+  };
+
+  deleteRoom = (id) => {
+    const roomDoc = doc(db, "Rooms", id);
+    return deleteDoc(roomDoc);
+  };
+
+  getAllRoom = () => {
+    return getDocs(roomCollectionRef);
+  };
+
+  getRoomById = (id) => {
+    const roomDoc = doc(db, "Rooms", id);
+    return getDoc(roomDoc);
+  };
+}
+
+export default new RoomService();
