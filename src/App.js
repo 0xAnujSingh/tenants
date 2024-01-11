78643d85934f606994bda695d823941c0b051ffb
Anuj Singh Solanki <anujsinghsolanki1999@gmail.com>
2024-01-11 12:14:52 +0530

    update paths and add new route ViewRequests

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import PrivateRoutes from "./components/PrivateRoutes";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Transactions from "./components/Transactions";
 import NewTenant from "./components/NewTenant";
+import NewRoom from "./components/NewRoom";
+import ViewRooms from "./components/ViewRooms";
+import AddNewTenant from "./components/AddNewTenant";
+import ViewRequests from "./components/ViewRequests";
 
 const App = () => {
   const [user, setUser] = useState(undefined); // undefined | null | User
+  //console.log(user)
 
   useEffect(() => {
     onAuthStateChanged(getAuth(), setUser);
   }, []);
 
   if (user === undefined) {
-    return <p>Loading</p>;
+    return <p>Loading...</p>;
   }
 
   return (
@@ -24,9 +29,13 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route element={<PrivateRoutes user={user} />}>
-            <Route path="/new-tenant" element={<NewTenant />}></Route>
+            {/* <Route path="/new-tenant" element={<NewTenant />}></Route> */}
             <Route path="/" element={<Data />}></Route>
             <Route path="/transactions/:id" element={<Transactions />}></Route>
+            <Route path="/rooms/new" element={<NewRoom />}></Route>
+            <Route path="/rooms/:id/new-tenant" element={<AddNewTenant />}></Route>
+            <Route path="/rooms/:id/requests" element={<ViewRequests />}></Route>
+            <Route path="/rooms" element={<ViewRooms />}></Route>
           </Route>
           <Route path="login" element={<Login user={user} />}></Route>
           <Route path="signup" element={<Signup user={user} />}></Route>
