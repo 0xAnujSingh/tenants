936f0dcaa8e7ec2926a80319b2cfcc10b0bcf11b
Anuj Singh Solanki <anujsinghsolanki1999@gmail.com>
2024-01-11 11:56:55 +0530

    owner will added the room and if you are the user then you will apply for that room to become a tenant or if you are the owner then you will see the requests of particular room

---
+++ src/components/ViewRooms.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, Table } from "react-bootstrap";
+import RoomService from "./RoomService";
+import { onSnapshot } from "firebase/firestore";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
+
+const ViewRooms = () => {
+  const outlet = useOutletContext();
+  const [rooms, setRooms] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getAllRooms();
+  }, []);
+
+  const getAllRooms = () => {
+    onSnapshot(RoomService.ref(), (snapshot) => {
+      //console.log(snapshot);
+      setRooms(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      //   if (outlet.user.tenantId !== null) {
+      //     alert("Room");
+      //   }
+    });
+  };
+
+  return (
+    <div>
+      <Container>
+        <Table>
+          <thead>
+            <tr>
+              <th>RoomNo</th>
+              <th>Rent</th>
+              <th>Unit</th>
+              <th>Address</th>
+              <th>Windows</th>
+              <th>RoomSize</th>
+              <th>Button</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rooms.map((ro) => {
+              return (
+                <tr>
+                  <td>{ro.RoomNo}</td>
+                  <td>{ro.Rent}</td>
+                  <td>{ro.Unit}</td>
+                  <td>{ro.Address}</td>
+                  <td>{ro.Windows}</td>
+                  <td>{ro.RoomSize}</td>
+                  <td>
+                    {outlet.user.uid === ro.Owner ? <Link to={`/rooms/${ro.id}/requests`}>View Requests</Link> : <Link to={`/rooms/${ro.id}/new-tenant`}>Apply</Link>} 
+                    
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
+
+export default ViewRooms;
